60c5900f11e303dfcd14804bf01edc2e4e2b1007
mako10k <mako10k@mk10.org>
2024-10-29 07:45:00 +0000

    refactoring

--- src/tuncat_net.c
+++ src/tuncat_net.c
@@ -1,4 +1,5 @@
 #include "tuncat_net.h"
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <net/if.h>
@@ -7,7 +8,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-int inet6_net_pton(int af, const char *cp, void *buf, size_t len) {
+/**
+ * **inet6_net_pton** : getting numeric form of IPv6 Address and Netmask
+ *
+ * @param af  address family (AF_INET6 only)
+ * @param cp  string form of IPv6 Address and Netmask
+ * @param buf buffer to store numeric form of IPv6 Address
+ * @param len length of buffer
+ * @return    netmask bits
+ */
+static int inet6_net_pton(int af, const char *cp, void *buf, size_t len) {
   if (af != AF_INET6) {
     errno = EAFNOSUPPORT;
     return -1;
@@ -41,15 +51,11 @@ end:
   return bits;
 }
 
-#ifdef inet_net_pton
-#undef inet_net_pton
-#endif
-
-extern int inet_net_pton(int af, const char *cp, void *buf, size_t len);
-
-int inet_net_pton_orig(int af, const char *cp, void *buf, size_t len) {
+int inet_net_pton(int af, const char *cp, void *buf, size_t len) {
   if (af == AF_INET)
+#undef inet_net_pton
     return inet_net_pton(af, cp, buf, len);
+#define inet_net_pton inet_net_pton_replaced
   if (af == AF_INET6)
     return inet6_net_pton(af, cp, buf, len);
   errno = EAFNOSUPPORT;
--- src/tuncat_net.h
+++ src/tuncat_net.h
@@ -2,21 +2,14 @@
 #define __TUNCAT_NET_H__
 
 #include "tuncat.h"
+#include <arpa/inet.h>
 #include <stddef.h>
 
-/**
- * **inet6_net_pton** : getting numeric form of IPv6 Address and Netmask
- *
- * @param af  address family (AF_INET6 only)
- * @param cp  string form of IPv6 Address and Netmask
- * @param buf buffer to store numeric form of IPv6 Address
- * @param len length of buffer
- * @return    netmask bits
- */
-int inet6_net_pton(int af, const char *cp, void *buf, size_t len);
+// replace inet_net_pton with inet_net_pton_orig to support IPv6
+#define inet_net_pton inet_net_pton_replaced
 
 /**
- * **inet_net_pton_orig** : getting numeric form of IPv4 or IPv6 Address and
+ * **inet_net_pton** : getting numeric form of IPv4 or IPv6 Address and
  * Netmask
  *
  * @param af  address family (AF_INET or AF_INET6)
@@ -25,8 +18,6 @@ int inet6_net_pton(int af, const char *cp, void *buf, size_t len);
  * @param len length of buffer
  * @return    netmask bits
  */
-int inet_net_pton_orig(int af, const char *cp, void *buf, size_t len);
-
-#define inet_net_pton inet_net_pton_orig
+int inet_net_pton(int af, const char *cp, void *buf, size_t len);
 
 #endif // __TUNCAT_NET_H__
